959cd3c377cb7b5e5b545fe04759f2055678e75d
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-07-13 22:10:04 -0300

    aa

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-def comb(x,y):
-    import math
-    fatx = math.factorial(x)
-    faty = math.factorial(y)
-    fatyx = math.factorial(x-y)
-    combinacao = fatx / (fatyx * faty)
-    return combinacao
-
-
-
-
-
+import funcoes
 print("Seja Bem vindo!")
-menu1=int(input("Digite 1 para Probabilidade Binomial individual\nDigite 2 para Probabilidade Binomial Acumulada\n = "))
-while True:
-    if menu1 != 1 and menu1 != 2:
-        print("Opção invalida!")
-        menu1=int(input("Digite 1 para Probabilidade Binomial individual\nDigite 2 para Probabilidade Binomial Acumulada\n = "))
-    else:
-        break
+menu1=funcoes.menu()
 if menu1 == 1:
     x=float(input("Digite um valor para atribuir a X: "))
     p=float(input("Digite um valor absoluto para atribuir a P: "))
     n=float(input("Digite um valor para atribuir a N: "))
-    combinacao=comb(n,x)
-    proba=combinacao*(p**x)
+    fracasso = (1 - p)
+    combinacao=funcoes.comb(n,x)
+    proba=combinacao*((p**x)*(fracasso**(n-x)))
+    print(proba)
 else:
     x=float(input("Digite um valor para atribuir a X: "))
     p=float(input("Digite um valor absoluto para atribuir a P: "))
