4fa6168882971c19a17a2ba61a576b52eeffeffc
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-07-13 21:22:52 -0300

    imp menu

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-teste
+print("Seja Bem vindo!")
+menu1=int(input("Digite 1 para Probabilidade Binomial individual\nDigite 2 para Probabilidade Binomial Acumulada\n = "))
+while True:
+    if menu1 != 1 and menu1 != 2:
+        print("Opção invalida!")
+        menu1=int(input("Digite 1 para Probabilidade Binomial individual\nDigite 2 para Probabilidade Binomial Acumulada\n = "))
+    else:
+        break
+
