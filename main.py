d3abda4bce7dc96b263ae69ead4cbb91accdb906
Geovane Machado <geovanemachado9@gmail.com>
2021-07-13 22:22:36 -0300

    top

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import funcoes
+
 print("Seja Bem vindo!")
-menu1=funcoes.menu()
+menu1=funcoes.menu() #imprime o menu na tela
 if menu1 == 1:
     x=float(input("Digite um valor para atribuir a X: "))
     p=float(input("Digite um valor absoluto para atribuir a P: "))
     n=float(input("Digite um valor para atribuir a N: "))
     fracasso = (1 - p)
-    combinacao=funcoes.comb(n,x)
+    combinacao = funcoes.comb(n,x)
     proba=combinacao*((p**x)*(fracasso**(n-x)))
     print(proba)
 else:
