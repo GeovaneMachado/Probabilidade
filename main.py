8b805f302276b6052d7e82aa68d694d03f92ca3b
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-07-13 22:18:06 -0300

    cxc

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import funcoes
 print("Seja Bem vindo!")
-menu1=funcoes.menu()
+menu1=funcoes.menu() 
 if menu1 == 1:
     x=float(input("Digite um valor para atribuir a X: "))
     p=float(input("Digite um valor absoluto para atribuir a P: "))
     n=float(input("Digite um valor para atribuir a N: "))
-    fracasso = (1 - p)
-    combinacao=funcoes.comb(n,x)
-    proba=combinacao*((p**x)*(fracasso**(n-x)))
+    fracasso = (1 - p)  # probabilidade de fracasso
+    combinacao=funcoes.comb(n,x) # Chama a função que calcula a combinação de n elementos tomados x a x 
+    proba=combinacao*((p**x)*(fracasso**(n-x))) # calcula a probabilidade binomial individual
     print(proba)
 else:
     x=float(input("Digite um valor para atribuir a X: "))
