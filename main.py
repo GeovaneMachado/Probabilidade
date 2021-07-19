d4b720a48895f59417c808bf80ba62c42e286ded
lucasschmidt015 <lucas.schmidt015@gmail.com>
2021-07-19 19:53:40 -0300

    relatividade

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import funcoes
 print("Seja Bem vindo!")
 menu1=funcoes.menu() 
 if menu1 == 1:
-    x=float(input("Digite um valor para atribuir a X: "))
-    p=float(input("Digite um valor absoluto para atribuir a P: "))
-    n=float(input("Digite um valor para atribuir a N: "))
+    x=float(input("Digite o numero especificado de sucessos (x): "))
+    p=float(input("Digite o valor absoluto da probabilidade de sucesso (p): "))
+    n=float(input("Digite o numero de observações (n): "))
     fracasso = (1 - p)  # probabilidade de fracasso
     combinacao=funcoes.comb(n,x) # Chama a função que calcula a combinação de n elementos tomados x a x 
     proba=combinacao*((p**x)*(fracasso**(n-x))) # calcula a probabilidade binomial individual
-    print(proba)
+    relativo=(proba*100)/p
+    print(f"P(x={x})={relativo}")
 else:
-    x=float(input("Digite um valor para atribuir a X: "))
-    p=float(input("Digite um valor absoluto para atribuir a P: "))
-    n=float(input("Digite um valor para atribuir a N: "))
+    x=float(input("Digite o numero especificado de sucessos (x): "))
+    p=float(input("Digite o valor absoluto da probabilidade de sucesso (p): "))
+    n=float(input("Digite o numero de observações (n): "))
